35e5032116b050a221eaf105b63f4ebf50d1226c
Валентин <valmak04@gmail.com>
2021-09-23 18:23:00 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.print("Hello, " + name);
     }
 }
