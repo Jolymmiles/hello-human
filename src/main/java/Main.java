4da02861a0c5fb1f04da712d6d0af597361b6f79
Валентин <valmak04@gmail.com>
2021-09-23 18:23:48 +0400

    Done 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
-        System.out.print("Hello, " + name);
+        System.out.print("Hello, " + name + "!");
     }
 }
